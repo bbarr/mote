f3ba8db7d342dc8198c89b2d49623365fa691b1e
Damian Galarza <galarza.d@gmail.com>
2011-01-30 20:43:25 -0500

    Hash stringify_keys

--- lib/mote/extensions.rb
+++ lib/mote/extensions.rb
@@ -11,3 +11,26 @@ class BSON::ObjectId
     as_json.to_json
   end
 end
+
+# Open up the Hash class to provide a stringify keys method to sanitize
+# hashes before we create a Mote::Document.
+#
+# @see https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/hash/keys.rb
+class Hash
+
+  # Creates a new hash with keys stringified
+  def stringify_keys
+    dup.stringify_keys!
+  end
+
+  # Loop through all keys and create a string key for each one
+  # deleteing the old key and returning the hash
+  #
+  # @return [Hash] Stringified hash
+  def stringify_keys!
+    keys.each do |key|
+      self[key.to_s] = delete(key)
+    end
+    self
+  end
+end
