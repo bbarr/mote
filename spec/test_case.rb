c2977459dc52d1e984cd863b968de9d900a08d9d
Damian Galarza <galarza.d@gmail.com>
2011-02-07 18:09:01 -0500

    Using mote's pk factory

--- spec/test_case.rb
+++ spec/test_case.rb
@@ -1,11 +1,10 @@
 require File.expand_path "../../lib/mote.rb", __FILE__
 
-Mote.db = Mongo::Connection.new.db("mote_test")
+Mote.db = Mongo::Connection.new.db("mote_test", :pk => Mote::PkFactory)
 
 class Book < Mote::Document
   include Mote::Callbacks
 
   def my_before_save
-    p "before save"
   end
 end
