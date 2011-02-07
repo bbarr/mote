73badfc2dcf4d1b918673389c5d79f8689f72b4e
Damian Galarza <galarza.d@gmail.com>
2011-02-07 15:17:53 -0500

    Cursor updated to have a to_a method which will return an array of Mote::Documents

--- lib/mote/cursor.rb
+++ lib/mote/cursor.rb
@@ -9,12 +9,22 @@ module Mote
       @mongo_cursor = mongo_cursor
     end
 
+    # Loop through each cursor entry and create a Mote::Document for
+    # each document found in the cursor
     def each
       @mongo_cursor.each do |doc|
         yield(@obj_class.new(doc))
       end
     end
+
+    # Creates an array of Mote::Documents for the entire cursor
+    #
+    # @return [Array <Mote::Document>] Array of Mote::Documents for each entry found
+    def to_a
+      document_array = [].tap do |arr|
+        self.each { |doc| arr << doc }
+      end
+    end
+
   end
-
 end
-
