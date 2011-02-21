c52ba1aeaf9daa1a29458b65851810e3a07abb83
Damian Galarza <galarza.d@gmail.com>
2011-02-20 19:25:30 -0500

    Added simplecov for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
+require "bundler"
+Bundler.require(:default, :development, :test)
+
+require "simplecov"
+SimpleCov.start
+
 require File.join(File.dirname(__FILE__), "../lib/mote.rb")
 require File.join(File.dirname(__FILE__), "test_case.rb")
-
-require "rubygems"
-require "bundler"
-
-Bundler.require(:default, :development, :test)
 
 Rspec.configure do |config|
   config.after(:each) { Book.collection.drop }
