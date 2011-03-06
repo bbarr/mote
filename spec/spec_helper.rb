314ea6fee97d8d13e4ef3bdf049911e786531d56
Damian Galarza <galarza.d@gmail.com>
2011-03-05 19:01:27 -0500

    Spec helper and test case updated to use the configure routine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,10 @@ SimpleCov.start
 require File.join(File.dirname(__FILE__), "../lib/mote.rb")
 require File.join(File.dirname(__FILE__), "test_case.rb")
 
+Mote.configure do |config|
+  config.db = Mongo::Connection.new.db("mote_test", :pk => Mote::PkFactory)
+end
+
 Rspec.configure do |config|
   config.after(:each) { Book.collection.drop }
 end
--- spec/test_case.rb
+++ spec/test_case.rb
@@ -1,6 +1,4 @@
-require File.expand_path "../../lib/mote.rb", __FILE__
-
-Mote.db = Mongo::Connection.new.db("mote_test", :pk => Mote::PkFactory)
+#require File.expand_path "../../lib/mote.rb", __FILE__
 
 class Book < Mote::Document
   include Mote::Callbacks
